c9cda268f4a48bab96a8911c478f5fad3e8bc33f
Roy Kathurima <kathurimaroy@gmail.com>
2022-02-21 10:12:32 +0300

    running some tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,7 @@
-// const prettierConfig = require('./package.json').prettier;
-
 module.exports = {
   parser: '@typescript-eslint/parser',
-  //   extends: ['eslint-config-airbnb', 'prettier', 'eslint-config-prettier'],
   extends: ['eslint-config-diwala'],
-  //   plugins: ['eslint-plugin-prettier'],
   parserOptions: {
     project: './tsconfig.json',
   },
-  //   rules: {
-  //     'max-len': ['error', 110],
-  //     'prettier/prettier': ['error', prettierConfig],
-  //   },
-  //   overrides: [
-  //     {
-  //       files: ['**/*.tsx'],
-  //       rules: {
-  //         'import/prefer-default-export': 'off',
-  //         'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-  //         'react/prop-types': 'off',
-  //         'func-style': ['error', 'expression', { allowArrowFunctions: true }],
-  //         // additional styles
-  //         'react/react-in-jsx-scope': 'off',
-  //         'func-names': 'off',
-  //         'no-undef': 'off',
-  //         'import/extensions': 'off',
-  //         'import/no-unresolved': 'off',
-  //         'react/jsx-filename-extension': 'off'
-  //       },
-  //     },
-  //   ],
 };
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 
 const App = function () {
   const [count, setCount] = useState(0);
+  const name = "Kathurima"
   return (
     <div className="App">
       <header className="App-header">
